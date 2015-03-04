c40134d13066f2c56048107b95715cd62d5623db
dhaniwid <real.ramadhani.widodo@gmail.com>
2015-03-04 08:26:44 +0700

    Initial commit for dummy hotel

---
+++ app/views/amarterravilla/index.php
@@ -0,0 +1,146 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Amarterra Villas Bali Nusa Dua : A Luxury Private Villa &amp; Spa in Nusa Dua, Bali</title>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" type="text/css" media="all" href="calendar-blue.css" />
+<link rel="icon" type="image/x-icon" href="favicon.ico" />
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+
+<script type="text/javascript" src="fadeslideshow.js">
+
+/***********************************************
+* Ultimate Fade In Slideshow v2.0- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at http://www.dynamicdrive.com/ for this script and 100s more
+***********************************************/
+
+</script>
+
+<script type="text/javascript">
+
+var mygallery=new fadeSlideShow({
+	wrapperid: "fadeshow1", //ID of blank DIV on page to house Slideshow
+	dimensions: [960, 330], //width/height of gallery in pixels. Should reflect dimensions of largest image
+	imagearray: [
+		["images/amarterra_home_img.jpg", "", "", ""] //<--no trailing comma after very last image element!
+	],
+	displaymode: {type:'auto', pause:4000, cycles:0, wraparound:false},
+	persist: false, //remember last viewed slide and recall within same session?
+	fadeduration: 1500, //transition duration (milliseconds)
+	descreveal: "ondemand",
+	togglerid: ""
+})
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+</script>
+
+</head>
+<body onload="MM_preloadImages('images/facebook_on.png','images/twitter_on.png','images/tripadvisor_on.png','images/netlog_on.png','images/4shared_on.png','images/tumblr_on.png','images/wordpress_on.png','images/googleplus_on.png','images/accor_advantage_plus_on.png','images/le_club_accor_hotels_on.png')">
+<div id="wrap">
+	<div id="main">
+		<div id="header">
+        <img src="images/mgallery_logo.png" alt="MGallery - Memorable Hotels by Accor" class="mgallerylogo" height="139" width="71" />
+        <img src="images/amarterra_logo_gold.png" alt="Amarterra Villas Bali Nusa Dua" class="logo" height="115" width="190" />
+			<div id="topmenu">
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="about_us.html">About Us</a></li>
+					<li><a href="villas.html">Villas</a></li>
+					<li><a href="amarterra_spa.html">Amarterra Spa</a></li>
+					<li><a href="services_facilities.html">Restaurant, Services &amp; Facilities</a></li>
+					<li><a href="special_offers.html">Special Offers</a></li>
+					<li><a href="photo_gallery.html">Gallery</a></li>
+					<li><a href="contact.php">Contact Us</a></li>
+				</ul>
+         	</div><!--end topmenu -->
+		</div><!-- end header -->
+		<div id="mainimg">
+			<div id="fadeshow1"></div>
+	    </div><!--end mainimg -->
+		<div id="submenu">
+			<div id="booking">
+			<iframe src="http://localhost:9010/widget-vertical" height="367px" width="88%" style="position: relative;border-radius: 5px 5px 5px 5px;
+                          border: 1px solid #ebebeb;"></iframe>
+				<p>&nbsp;</p>
+			    <p><a href="http://www.accoradvantageplus.com" target="_blank" onmouseover="MM_swapImage('accor_advantage_plus','','images/accor_advantage_plus_on.png',1)" onmouseout="MM_swapImgRestore()"><img src="images/accor_advantage_plus_off.png" alt="Accor Advantage Plus" width="230" height="50" id="accor_advantage_plus" /></a></p>
+			    <p><a href="http://www.accorhotels.com/leclub" target="_blank" onmouseover="MM_swapImage('le_club_accor_hotels','','images/le_club_accor_hotels_on.png',1)" onmouseout="MM_swapImgRestore()"><img src="images/le_club_accor_hotels_off.png" alt="Le Club Accor Hotels" width="230" height="50" id="le_club_accor_hotels" /></a></p>
+	    	</div>
+		</div><!-- end submenu -->
+		<div id="content">
+			<h1>Amarterra Villas Bali Nusa Dua<br />MGallery Collection</h1>
+			<!-- <table border="0" cellpadding="0" cellspacing="0" width="100%">
+			<tbody>
+			<tr>
+			<td><a href="honeymoon_package.html"><img src="images/amarterra_honeymoon_package.png" alt="Honeymoon Package" height="125" width="290" /></a></td>
+			<td>&nbsp;</td></tr></tbody></table>
+			-->
+			<p>Amarterra Villas Bali Nusa Dua – a part of the MGallery boutique collection of hotels. 39 luxury private pool villas located within the heart of Nusa Dua, close to the beach and offering five star luxury service. Built in elegant style combining Balinese ethnicity, artistic touches and 21st century technology, all with modern comforts, this is perfect place for honeymooners, couples, and those wanting to escape the hustle and bustle of the world. </p><br />
+		</div><!-- end content -->
+	</div><!-- end main -->
+</div><!-- end wrap -->
+
+<div id="footer">
+	<div id="socialmediaicon"> 
+		<a href="http://www.facebook.com/pages/Amarterra-Villas-Spa-Nusa-Dua-Bali/406807272706459" target="_blank"><img src="images/facebook.png" alt="facebook" width="32" height="32" id="facebook" title="facebook"/></a>&nbsp;
+		<a href="http://www.tripadvisor.com/Hotel_Review-g297698-d3515952-Reviews-Amarterra_Villas_Bali_Nusa_Dua_MGallery_Collection-Nusa_Dua_Nusa_Dua_Peninsula_Bali.html" target="_blank"><img src="images/tripadvisor.png" alt="tripadvisor" width="32" height="32" id="tripadvisor" title="tripadvisor"/></a>&nbsp;
+		<a href="http://www.youtube.com/watch?v=-oYkQ-9-tno" target="_blank"><img src="images/youtube.png" alt="youtube" width="32" height="32" id="youtube" title="youtube"/></a>&nbsp;
+		<br><br>
+	</div><!-- end social media icon -->	
+	
+	
+	<div id="copyright">&copy; 2014 <a href="http://www.amarterravilla.com/">AMARTERRA VILLAS BALI NUSA DUA</a>&nbsp;|&nbsp;<a href="http://www.mgallery.com/8971" target="_blank">MGALLERY</a>&nbsp;|&nbsp;<a href="site_map.html">SITE MAP</a><br /><br /></div><!-- end copyright -->
+	
+	<table border="0" cellpadding="0" cellspacing="0" style=" width:100%; align:center;">
+		<tr>
+			<td align="right"><div id="TA_certificateOfExcellence564" class="TA_certificateOfExcellence"><ul id="KLxR2eHpLK" class="TA_links xP6jNsPdrz5"><li id="9mt8uArME" class="Ojy0OddTfw"><a target="_blank" href="http://www.tripadvisor.com/"><img src="http://www.tripadvisor.com/img/cdsi/img2/awards/CoE2014_WidgetAsset-14348-2.png" alt="TripAdvisor" class="widCOEImg" id="CDSWIDCOELOGO"/></a></li></ul></div><script src="http://www.jscache.com/wejs?wtype=certificateOfExcellence&uniq=564&locationId=3515952&lang=en_US&year=2014"></script> 
+</td>
+			<td align="center" width="180px"><a href="http://www.luxuryhotelawards.com/listings/amarterra-villas-bali-nusa-dua-mgallery-collection"><img src="images/2015_WLHA_Nominee_Logo.jpg" title="http://www.luxuryhotelawards.com/listings/amarterra-villas-bali-nusa-dua-mgallery-collection" alt="2015_WLHA_Nominee_Logo"/></a>
+</td>
+			<td align="left"><a href="http://www.holidaycheck.com/hi/ae59147b-d494-3ae2-8669-f47ecb661a30"><img src="images/quality_selection_2014.png" title="http://www.holidaycheck.com/hi/ae59147b-d494-3ae2-8669-f47ecb661a30" alt="Amarterra Villas Bali Nusa Dua"/></a>
+</td>
+		</tr>
+	</table>
+	
+</div><!-- end footer -->
+
+
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38432031-1']);
+  _gaq.push(['_setDomainName', 'amarterravilla.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</body>
+</html>
